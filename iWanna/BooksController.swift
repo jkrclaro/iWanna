76a98fcdfbfe6f38156ba4d4254cfa8b20107175
John Claro <jkrclaro@gmail.com>
2016-03-18 21:57:38 +0000

    Add Controllers for Books, Movies and Songs

---
+++ iWanna/BooksController.swift
@@ -0,0 +1,9 @@
+//
+//  BooksController.swift
+//  iWanna
+//
+//  Created by John Claro on 18/03/2016.
+//  Copyright © 2016 jkrclaro. All rights reserved.
+//
+
+import Foundation
---
+++ iWanna/MoviesController.swift
@@ -0,0 +1,9 @@
+//
+//  MoviesController.swift
+//  iWanna
+//
+//  Created by John Claro on 18/03/2016.
+//  Copyright © 2016 jkrclaro. All rights reserved.
+//
+
+import Foundation
